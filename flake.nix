9485610af7e00a516fc8656904596494186eb0e1
M Rifqi Al Furqon <rifqi.alfurqon@doku.com>
2025-12-21 23:47:22 +0700

    change outputs vm

--- flake.nix
+++ flake.nix
@@ -13,21 +13,17 @@
     disko,
     ...
   }: let
-    system = "aarch64-linux";
     lib = nixpkgs.lib;
-
-    mkHost = modules:
-      lib.nixosSystem {
-        inherit system;
-        modules = modules;
-      };
   in {
     nixosConfigurations = {
-      vm = mkHost [
-        disko.nixosModules.disko
-        ./hosts/vm/disko.nix
-        ./hosts/vm/configuration.nix
-      ];
+      vm = lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./hosts/vm/disko.nix
+          ./hosts/vm/configuration.nix
+        ];
+      };
 
       atlas = lib.nixosSystem {
         system = "x86_64-linux";
