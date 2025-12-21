4a655aa66d674e10c82e4c77d1e543ffa581596f
M Rifqi Al Furqon <rifqi.alfurqon@doku.com>
2025-12-22 01:55:27 +0700

    add node exporter modules

---
+++ modules/monitoring/node-exporter.nix
@@ -0,0 +1,58 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.features.monitoring.nodeExporter;
+in {
+  # ============================================================================
+  # MODULE OPTIONS
+  # ============================================================================
+
+  options.features.monitoring.nodeExporter = {
+    enable = lib.mkEnableOption "Prometheus Node Exporter";
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 9100;
+      description = "Port for Node Exporter metrics";
+    };
+
+    enabledCollectors = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [
+        "systemd"
+        "textfile"
+        "filesystem"
+        "loadavg"
+        "meminfo"
+        "netdev"
+        "stat"
+      ];
+      description = "List of enabled collectors";
+    };
+  };
+
+  # ============================================================================
+  # MODULE IMPLEMENTATION
+  # ============================================================================
+
+  config = lib.mkIf cfg.enable {
+    # --------------------------------------------------------------------------
+    # NODE EXPORTER SERVICE
+    # --------------------------------------------------------------------------
+
+    services.prometheus.exporters.node = {
+      enable = true;
+      port = cfg.port;
+      enabledCollectors = cfg.enabledCollectors;
+    };
+
+    # --------------------------------------------------------------------------
+    # FIREWALL CONFIGURATION
+    # --------------------------------------------------------------------------
+
+    networking.firewall.allowedTCPPorts = [cfg.port];
+  };
+}
